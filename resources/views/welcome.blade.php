39f3a0bacddb4b89bce507e57efe0c818e57caa6
Okorie Emmanuel <teymee17@gmail.com>
2020-12-05 21:47:24 +0100

    customer changes 3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <section class="home-slider js-fullheight owl-carousel">
 
 
-        <div class="slider-item js-fullheight" style="background-image:url(website/images/headline3.jpg);">
+        <div class="slider-item js-fullheight" style="background-image:url(website/images/headline1.jpg);">
             <div class="overlay"></div>
             <div class="container-fluid">
                 <div class="row no-gutters slider-text slider-text-2 js-fullheight align-items-center justify-content-center" data-scrollax-parent="true">
@@ -18,7 +18,7 @@
         </div>
 
 
-        <div class="slider-item js-fullheight" style="background-image:url(website/images/bg_4.jpg);">
+        <div class="slider-item js-fullheight" style="background-image:url(website/images/headline2.jpg);">
             <div class="overlay"></div>
             <div class="container-fluid">
                 <div class="row no-gutters slider-text slider-text-2 js-fullheight align-items-center justify-content-center" data-scrollax-parent="true">
