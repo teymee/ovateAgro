f38180dd42db5848bcdcf36ade314f167f7f1486
Okorie Emmanuel <teymee17@gmail.com>
2020-12-05 11:34:44 +0100

    customer changes

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -187,7 +187,7 @@
             <div class="col-md-12 text-center">
 
                 <p> <span style="font-size: 20px; color: ghostwhite "><strong>Ovate Agro Center</strong></span>
-                    <br>Site made by <a href="http://teymee.herokuapp.com/" style="color:lawngreen">Teymee</a>
+                    <br>Site made by <a href="http://teymee.herokuapp.com/" style="color:lawngreen" target="_blank">Teymee</a>
                     <br>Copyright &copy; <script>document.write(new Date().getFullYear()); </script></p>
 {{--                <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->--}}
 {{--                    Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>--}}
